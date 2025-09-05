8d5f8bf7282e7f3e7a314c0d523d89fe0041ba9e
23f2003040 <23f2003040@ds.study.iitm.ac.in>
2025-09-05 22:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from backend.models import*
+import os
 
 app=None
 
@@ -16,4 +17,5 @@ app=init_app()
 from backend.controllers import *
 
 if __name__=="__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
